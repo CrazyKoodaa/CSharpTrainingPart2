b995a9c9a91c219cd20c1afce98f6d78cf0a727a
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-01 15:46:11 +0200

    Add project files.

---
+++ CSharp Training Part 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Training_Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
