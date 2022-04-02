a8e4ef5f0ec16c7151279ba47cd2826f350fb671
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-02 16:13:04 +0200

    writing notes about linq queries

--- CSharp Training Part 2/Program.cs
+++ CSharp Training Part 2/Program.cs
@@ -7,6 +7,17 @@ namespace CSharp_Training_Part_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            /*
+                from comic in comics
+                orderby prices[comic.Issue] descending
+                group comic by CalculatePriceRange(comic) into priceGroup
+                select priceGroup;
+
+                from comic in comics
+                orderby comic.Issue descending
+                join review in reviews on comic.Issue equals review.Issue
+                select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+            */
         }
     }
 }
