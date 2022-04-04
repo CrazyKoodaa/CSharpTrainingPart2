fdb46e3d75f34515d5b0afa23d45cadad9eea0e5
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-04 14:54:17 +0200

    refactoring clown to use lambda. Incl. f.e. { Get => 1.5f }

---
+++ Chapter9/Clowns/Clowns/TallGuy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clowns
+{
+    class TallGuy : IClown
+    {
+        public string Name;
+        public int Height;
+        public string FunnyThingIHave => "big red shoes";
+
+        public void Honk() => Console.WriteLine("Honk Honk!");
+
+        public void TalkAboutYourself() => Console.WriteLine($"My name is {Name} and I'm {Height} inches tall");
+    }
+
+}
