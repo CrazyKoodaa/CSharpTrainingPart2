578d626ef17bb5c2475df1002e82225beae9d295
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-04 14:40:41 +0200

    refactoring code with lambda's

---
+++ Chapter9/lambdaTestDrive/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lambdaTestDrive
+{
+    class Program
+    {
+        static Random random => new Random();
+        
+        /*
+        static double GetRandomDouble(int max) 
+        {
+            return max * random.NextDouble();
+        }
+        */
+        // is the same as
+        static double GetRandomDouble(int max) => max * random.NextDouble();
+
+        /*
+        static void PrintValue(double d)
+        {
+            Console.WriteLine($"The value is {d:0.0000}");
+        }
+        */
+        // is the same as
+        static void PrintValue(double d) => Console.WriteLine($"The value is {d:0.0000}");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var value = Program.GetRandomDouble(100);
+            Program.PrintValue(value);
+        }
+    }
+}
