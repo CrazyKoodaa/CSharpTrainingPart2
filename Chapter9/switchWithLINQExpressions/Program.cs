e6276322e4c9c633af3a98c826f73e098295ea64
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-04 15:21:23 +0200

    switch to lambda Expressions

---
+++ Chapter9/switchWithLINQExpressions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace switchWithLINQExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // the next few lines:
+            var score = 0;
+            switch (card.Suit)
+            {
+                case Suits.Spades:
+                    score = 6;
+                    break;
+                case Suits.Hearts:
+                    score = 4;
+                    break;
+                default:
+                    score = 2;
+                    break;
+            }
+
+            // to this:
+            var score = card.Suit switch
+            {
+                Suits.Spades => 6,
+                Suits.Hearts => 4,
+                _ => 2,
+            };
+        }
+    }
+
+    internal class card
+    {
+    }
+}
