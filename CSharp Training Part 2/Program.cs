dd8b64ac2093f814209685a04e7cde5bbbc762e5
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-09 15:02:49 +0200

    refactored LINQ Queries to chained Methods

--- CSharp Training Part 2/Program.cs
+++ CSharp Training Part 2/Program.cs
@@ -17,6 +17,23 @@ namespace CSharp_Training_Part_2
                 orderby comic.Issue descending
                 join review in reviews on comic.Issue equals review.Issue
                 select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+
+            either this:
+                var score = 0;
+                switch (card.Suit)
+                {
+                    case Suits.Spades: score = 6; break;
+                    case Suits.Hearts: score = 4; break;
+                    default: score = 2; break;
+                }
+            or with switch Expression style:
+
+                var score = card.Suit switch
+                {
+                    Suits.Spades => 6,
+                    Suits.Hearts => 4,
+                    _ => 2,
+                };
             */
         }
     }
--- Chapter9/jimmyLINQ/ComicAnalyzer.cs
+++ Chapter9/jimmyLINQ/ComicAnalyzer.cs
@@ -14,21 +14,41 @@ namespace jimmyLINQ
 
         public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(IEnumerable<Comic> comics, IReadOnlyDictionary<int, decimal> prices)
         {
-            return 
-                from comic in comics
-                orderby prices[comic.Issue] // descending
-                group comic by CalculatePriceRange(comic, prices) into priceGroup
-                select priceGroup;
+
+            var temp =
+                comics
+                .OrderBy(comic => prices[comic.Issue])
+                .GroupBy(comic => CalculatePriceRange(comic, prices));
+
+            var temp2 = from comic in comics
+                        orderby prices[comic.Issue] // descending
+                        group comic by CalculatePriceRange(comic, prices) into priceGroup
+                        select priceGroup;
+
+            return temp;
 
         }
 
         public static IEnumerable<string> GetReviews(IEnumerable<Comic> comics, IEnumerable<Review> reviews)
         {
-            return 
+            var temp1 = 
+                comics
+                .OrderBy(comic => comic.Issue)
+                .Join(
+                    reviews,
+                    review => review.Issue,
+                    comic => comic.Issue,
+                    (comic, review) => $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+
+            var temp2 =
                 from comic in comics
                 orderby comic.Issue //descending
                 join review in reviews on comic.Issue equals review.Issue
                 select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+
+
+            return temp1;
+                
         }
     }
 }
