b2bae132c54c5608106ecbcf296fb6ecfde6c979
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-10 13:46:44 +0200

    Example of Yield and IEnumerator the Power of 2

---
+++ Yield Return the Power of 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Yield_Return_the_Power_of_2
+{
+    class PowersOfTwo : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            var maxPower = Math.Round(Math.Log(int.MaxValue, 2));
+            for (int i = 2; i <= maxPower; i++)
+            {
+                yield return (int)Math.Pow(2,i);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var counting = new PowersOfTwo();
+            foreach (var items in counting)
+                Console.Write(items + " ");
+        }
+    }
+}
