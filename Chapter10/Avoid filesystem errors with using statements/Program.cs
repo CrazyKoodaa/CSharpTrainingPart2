c1c315454ab568c5d8108aea835428f3602e0e55
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-05-04 10:16:54 +0200

    Avoid filesystem errors with using

---
+++ Chapter10/Avoid filesystem errors with using statements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Avoid_filesystem_errors_with_using_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var sw = new StreamWriter("secret_plan.txt"))
+            {
+                sw.WriteLine("After this line the file will be closed automatically");
+            }
+
+
+            // Stacking is also allowed
+            using (var reader = new StreamReader("secret_plan.txt"))
+            using (var writer = new StreamWriter("email.txt"))
+            {
+                // blah blah blah
+            }
+
+        }
+    }
+}
