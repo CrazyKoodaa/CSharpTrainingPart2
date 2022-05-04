6b67d1118566188f210a55e7d3937539167a73e0
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-05-04 15:37:35 +0200

    using using nested with memorystream and streamwriter

---
+++ Chapter10/MemoryStream/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MemoryStreamApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var ms = new MemoryStream())
+            {
+                using (var sw = new StreamWriter(ms))
+                {
+                    sw.WriteLine($"The value is {123.45678:0.00}");
+
+                }
+                Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+            }       
+
+        }
+    } 
+}
+          
