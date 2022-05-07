601d30955d8d4c9c3791e86fed2d23e7f5465f06
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-05-07 11:43:30 +0200

    extending sealed classes with methods

---
+++ Chapter11/extendExistingClassesWithNewMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace extendExistingClassesWithNewMethods
+{
+    sealed class OrdinaryHuman
+    {
+        private int age;
+        int weigth;
+
+        public OrdinaryHuman(int weight)
+        {
+            this.weigth = weight;
+        }
+
+        public void GoToWork() { /* code to go to work */ }
+        public void PayBills() { /* code to pay bills */ }
+    }
+
+    static class AmazeballsSerum
+    {
+        public static string BreakWalls(this OrdinaryHuman h, double wallDensity)
+        {
+            return ($"I broke through a wall of {wallDensity} density.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            OrdinaryHuman steve = new OrdinaryHuman(185);
+            Console.WriteLine(steve.BreakWalls(89.2));
+
+            
+        }
+    }
+}
+             
