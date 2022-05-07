78b314942ea3fae697f31fd0c0cfc89bf3ed7370
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-05-07 10:20:06 +0200

    struct vs objects; heap vs stack

---
+++ Chapter11/structVsObjects/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace structVsObjects
+{
+    /* 
+     * Structs are value types VW object are reference types  
+     */
+    internal class Program
+    {
+        public struct Dog
+        {
+            public string Name { get; set; }
+            public string Breed { get; set; }
+            public Dog(string name, string breed)
+            {
+                this.Name = name;
+                this.Breed = breed;
+            }
+            public void Speak()
+            {
+                Console.WriteLine($"My name is {Name} and I'm a {Breed}");
+            }
+        }
+                    
+        public class Canine 
+        { 
+            public string Name { get; set; }
+            public string Breed { get; set; }
+            public Canine(string name, string breed)
+            {
+                this.Name = name;
+                this.Breed = breed;
+            }
+            public void Speak()
+            {
+                Console.WriteLine($"My name is {Name} and I'm a {Breed}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Canine spot = new Canine("Spot", "pug");
+            Canine bob = spot;
+            bob.Name = "Spike";
+            bob.Breed = "beagle";
+
+            spot.Speak();
+
+            Dog jake = new Dog("Jake", "poodle");
+            Dog betty = jake;
+            betty.Name = "Betty";
+            betty.Breed = "pit bull";
+            jake.Speak();
+              
+        }
+    }
+}
